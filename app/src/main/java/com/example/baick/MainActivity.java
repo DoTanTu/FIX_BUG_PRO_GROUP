adeba4efa9c5f441336e42c8950a4bbc521c2f6f
unknown <thanhcuongrmr@gmail.com>
2022-11-29 21:55:31 +0700

    up menu

--- app/src/main/java/com/example/baick/MainActivity.java
+++ app/src/main/java/com/example/baick/MainActivity.java
@@ -7,6 +7,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -62,6 +64,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.bottom_nav,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id==R.id.menu){
+           Intent intent = new Intent(MainActivity.this,Menu.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 
 
---
+++ app/src/main/java/com/example/baick/Menu.java
@@ -0,0 +1,17 @@
+package com.example.baick;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.header);
+
+    }
+}
