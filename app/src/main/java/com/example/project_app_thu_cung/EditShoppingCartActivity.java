40761134ba38d395f5e343d7ae56e1ff76f6626f
DoTanTu <95263083+DoTanTu@users.noreply.github.com>
2022-12-02 06:50:26 +0700

    Do Tan Tu

---
+++ app/src/main/java/com/example/project_app_thu_cung/EditShoppingCartActivity.java
@@ -0,0 +1,13 @@
+package com.example.project_app_thu_cung;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_shopping_cart);
+    }
+}
